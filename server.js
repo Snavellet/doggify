09ba8ba89d295027410b65ab1d34efdd06aa1145
Colin Saari <snavellet@gmail.com>
2019-09-24 23:55:15 +0700

    Updated server.js

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const app = require('./app');
 
-app.listen('8000', () => {
-    console.log('App running on port 3000');
+const port = 8000;
+
+app.listen(port, '165.22.109.71', () => {
+    console.log(`App running on port ${port}...`);
 });
