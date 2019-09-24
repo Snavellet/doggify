60a9d1206d9edb628c9fbc402d977f793877a7c6
Colin Saari <snavellet@gmail.com>
2019-09-24 23:57:55 +0700

    Updated serverjs

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const app = require('./app');
 
 const port = 8000;
 
-app.listen(port, '165.22.109.71', () => {
+app.listen(port, '157.245.63.169', () => {
     console.log(`App running on port ${port}...`);
 });
